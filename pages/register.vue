85e5e917677be91150a1efbcf7a41487ccdd808e
sivabalanmano <siva08082001@gmail.com>
2024-01-02 07:25:43 +0530

    valid indication

--- pages/register.vue
+++ pages/register.vue
@@ -8,34 +8,44 @@
         <div class="p-2 items-center justify-center ">
             
             Name <br> <input  v-model="name" type="text" placeholder="Name" class="px-2 rounded-lg required: "
-         :class="isColor1 ? 'border-2 border-green-500' : 'border-2 border-red-500 hi'" >
+         :class="isColor1 ? 'border-2 border-green-500' : 'border-2 border-red-500'" > <br>
+         <span class="text-sm text-red-500" 
+            :class="isColor1 ? 'hidden' : 'success'" >Please Enter The Value</span>
          </div>
     </div>
     <div class="flex  items-center justify-center">
         <div class="p-2">
             
             E-Mail <br> <input v-model="email" type="e-mail" placeholder="E-Mail" class="px-2  rounded-lg border
-            " :class="isColor2 ? 'border-2 border-green-500' : 'border-2 border-red-500 '" >
+            " :class="isColor2 ? 'border-2 border-green-500' : 'border-2 border-red-500 '" > <br>
+            <span class="text-sm text-red-500" 
+            :class="isColor2 ? 'hidden' : 'success'" >Please Enter The Value</span>
         </div>
     </div>
     <div class="flex  items-center justify-center">
         <div class="p-2">
             Phone  <br> <input v-model="phone" type="Number" placeholder="Phone" class="px-2  rounded-lg border"
-            :class="isColor3 ? 'border-2 border-green-500' : 'border-2 border-red-500'" >
+            :class="isColor3 ? 'border-2 border-green-500' : 'border-2 border-red-500'" > <br>
+            <span class="text-sm text-red-500" 
+            :class="isColor3 ? 'hidden' : 'success'" >Please Enter The Value</span>
         </div>
     </div>
     <div class="flex  items-center justify-center">
         <div class="p-2">
            
             Password <br> <input v-model="password" type="password" placeholder="Password" class="px-2  rounded-lg border"
-            :class="isColor4 ? 'border-2 border-green-500' : 'border-2 border-red-500'" >
+            :class="isColor4 ? 'border-2 border-green-500' : 'border-2 border-red-500'" > <br>
+            <span class="text-sm text-red-500" 
+            :class="isColor4 ? 'hidden' : 'success'" >Please Enter The Value</span>
         </div>
     </div>
     <div class="flex  items-center justify-center">
         <div class="p-2">
             
             Re-Password <br>   <input v-model="repassword" type="password" placeholder="Re-Password" class="px-2  rounded-lg border"
-            :class="isColor5 ? 'border-2 border-green-500' : 'border-2 border-red-500'" >
+            :class="isColor5 ? 'border-2 border-green-500' : 'border-2 border-red-500' " >  <br>
+            <span class="text-sm text-red-500" 
+            :class="isColor5 ? 'hidden' : 'success'" >Please Enter The Value</span>
         </div>
     </div>
         <div class="p-2 flex items-center justify-center">
@@ -63,7 +73,7 @@
                 isColor2:true,
                 isColor3:true,
                 isColor4:true,
-                isColor5:true
+                isColor5:true,
             }
         },
         methods:{
